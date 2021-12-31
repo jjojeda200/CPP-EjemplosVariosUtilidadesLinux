6f1a12eabfe49e483b8f149f3b5c17421261f7a3
José Juan Ojeda Granados <jotafirebase@gmail.com>
2021-12-31 19:27:14 +0000

    Archivos actualizados

--- Linux-Hilos-pthread01.cpp
+++ Linux-Hilos-pthread01.cpp
@@ -11,27 +11,52 @@
     gcc Linux-Hilos-pthread00.cpp -pthread -o Linux-Hilos-pthread
 */
 #include <pthread.h>
-#include <sys/stat.h>
+#include <fstream>
+//#include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <errno.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // int pthread_join(pthread_t thread, void **retval);
 
-
 void *funcionThread(void *argValor)
 {
-    
-    system("xfce4-terminal");
+    int numeroLineas = *((int*)argValor);
+    int descriptorArchivo;
+    char bufer[] = "Nueva línea a añadir al archivo de prueba\n";
+
+    printf("ID de hilo Uno: %u\n", (unsigned int)pthread_self());
+
+    for (int i = 0; i < numeroLineas; i++)
+    {
+        descriptorArchivo = open("./prueba.txt", O_CREAT | O_WRONLY | O_APPEND, 0644);
+        write(descriptorArchivo, bufer, sizeof(bufer)-1);
+        if (descriptorArchivo == -1)
+        {
+            printf("[Error] %d: %s \n", errno, strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+        close(descriptorArchivo);
+    }
+
     return 0;
 }
 
 int main(int argc, char *argv[])
 {
-    pthread_t hiloUno; // Definicion de variable tipo pthread identificador del hilo
-    int argValor;
+    system("clear");
+    
+
+    if (argc != 2)
+    {
+        printf("%s \e[0;33mUso: Indica el número de lineas a añadir al archivo\e[0m\n", argv[0]);
+        return 1;
+    }
+
+    pthread_t hiloUno;            // Definición de variable tipo pthread identificador del hilo
+    int argValor = atoi(argv[1]); // Convertimos el char de argv en integer
     int errorHilo;
 
     /*
@@ -41,18 +66,18 @@ int main(int argc, char *argv[])
         void *(*start_routine)(void *),
         void *restrict arg);
     */
-     errorHilo = pthread_create(
-                        &hiloUno,       // Puntero al identificador
-                        NULL,           // Atributos del hilo, NULL = los por defecto
-                        &funcionThread, // Rutina a ejecutar al crear el hilo
-                        &argValor);     // Argumento (unico) que se pasa a la rutina
+    errorHilo = pthread_create(
+        &hiloUno,       // Puntero al identificador
+        NULL,           // Atributos del hilo, NULL = los por defecto
+        &funcionThread, // Rutina a ejecutar al crear el hilo
+        &argValor);     // Argumento (único) que se pasa a la rutina
 
     if (errorHilo != 0)
     {
-        printf("Error creando el hilo, codigo %i \n", errorHilo);
+        printf("Error creando el hilo, código %i \n", errorHilo);
         return -1;
     }
-
+    pthread_join(hiloUno, NULL);
 
     return 0;
 }
